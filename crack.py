f1bda527d17fe59c17f6f2441cec4144ed44c632
Eraxty <hardikrajotiyamars@gmail.com>
2026-07-17 16:53:10 +0530

    CLI

---
+++ crack.py
@@ -0,0 +1,48 @@
+import subprocess
+import sys
+from crypto import generate_key, encrypt, decrypt
+
+def main():
+    while True:
+        print("=============================")
+        print("CrackLab")
+        print("=============================")
+        print("1. Crack cipher")
+        print("2. Encrypt")
+        print("3. Decrypt")
+        print("4. Exit")
+
+        choice = input("> ").strip()
+
+        if choice == "1":
+            subprocess.run([sys.executable, "main.py"])
+
+        elif choice == "2":
+                    message = input("\nMessage: ")
+                    key = generate_key()
+                    encrypted_text = encrypt(message, key)
+                    print("\nEncrypted:")
+                    print(encrypted_text)
+                    print("\nKey:")
+                    print(key.hex())
+
+        elif choice == "3":
+            encrypted_text = input("\nEncrypted text: ").strip()
+            key_hex = input("Key: ").strip()
+            try:
+                key = bytes.fromhex(key_hex)
+                plaintext = decrypt(encrypted_text, key)
+                print("\nDecrypted: ")
+                print(plaintext)
+
+            except Exception:
+                print("Invalid key")
+        
+        elif choice == "4":
+            break
+
+        else:
+            continue
+
+if __name__ == "__main__":
+    main()
